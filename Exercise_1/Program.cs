e7e8a66af38fe84fa827b3c315875f0693a6fee4
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-11-24 10:50:00 +0300

    Exercise_1 completed

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = GetNumber("Enter your first number ");
+int b = GetNumber("Enter your second number ");
+WhichIsBigger(a, b);
+
+
+void WhichIsBigger(int a, int b)
+{
+    int max = 0;
+    if (a > b)
+    {
+        max = a;
+    }
+    else
+    {
+        max = b;
+    }
+    Console.WriteLine($"a = {a}; b = {b} ->  max = {max}");
+}
+
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
