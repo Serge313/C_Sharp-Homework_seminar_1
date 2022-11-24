009aef01b79bf54417736c37cbaf4482fd25a595
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-11-24 12:37:04 +0300

    Exercise_3 completed

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,26 @@
+﻿int yourNumber = GetNumber("Enter your number ");
+EvenOrOdd(yourNumber);
+
+
+void EvenOrOdd(int a)
+{
+    if (a % 2 == 0 & a / 2 != 0)
+    {
+        Console.WriteLine($"{a} -> \"yes\"");
+    }
+    if (a % 2 != 0)
+    {
+        Console.WriteLine($"{a} -> \"no\"");
+    }
+    if (a == 0)
+    {
+        Console.WriteLine("Choose a number not equal to 0");
+    }
+}
+
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
